cace35238a7d996f493689b2443673cf6ad83456
Nachiket Patil <166910327+NachiketPatil26@users.noreply.github.com>
2025-02-02 01:13:15 +0530

    Mobile view updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,34 @@
 /* eslint-disable no-unused-vars */
 
-import React from "react"
-import Navbar from "./components/Navbar"
-import ThreeBG from "./components/ThreeBG"
+import React, { useState, useEffect } from "react";
+import Navbar from "./components/Navbar";
+import ThreeBG from "./components/ThreeBG";
+import MobileView from "./components/MobileView";
 
 const App = () => {
+  const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 768);
+    };
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   return (
     <div>
-      <Navbar />
-      <ThreeBG />
+      {isMobile ? (
+        <MobileView />
+      ) : (
+        <>
+          <Navbar />
+          <ThreeBG />
+        </>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
